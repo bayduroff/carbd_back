da9130b65eebb9bd66372f29ac36ca20d116f291
Ilya Baydurov <ibajdurov@vsegda-da.com>
2023-08-31 03:38:33 +0300

    * added exceptions * added unique passport check

--- src/main/java/org/gus/carbd/service/PersonService.java
+++ src/main/java/org/gus/carbd/service/PersonService.java
@@ -2,6 +2,7 @@ package org.gus.carbd.service;
 
 import org.gus.carbd.entity.Person;
 import org.gus.carbd.entity.Vehicle;
+import org.gus.carbd.exception.ResourceNotFoundException;
 import org.gus.carbd.repository.PersonRepository;
 import org.gus.carbd.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,24 +22,27 @@ public class PersonService {
     @Autowired
     private VehicleRepository vehicleRepository;
 
-    //ищет всех пользователей
     public List<Person> getPeopleList() {
         return personRepository.findAll();
     }
 
     public Person getPersonById(int id) {
         Optional<Person> result = personRepository.findById(id);
-
-        Person person = new Person();
-
+        Person person;
         if (result.isPresent()) {
             person = result.get();
-        }
+        } else throw new ResourceNotFoundException("Did not find person id - " + id);
 
         return person;
     }
 
     public void addPerson(Person person) {
+        var allPeople = personRepository.findAll();
+        for (Person basePerson : allPeople) {
+            if (basePerson.getPassport().equals(person.getPassport()))
+                throw new RuntimeException("Person with passport: " + person.getPassport() + " already exists");
+        }
+
         personRepository.save(person);
     }
 
@@ -48,13 +52,12 @@ public class PersonService {
 
     public void editPersonById(int id, Person changedPerson) {
         Optional<Person> result = personRepository.findById(id);
-
-        Person person = new Person();
+        Person person;
         if (result.isPresent()) {
             person = result.get();
-        }
+        } else throw new ResourceNotFoundException("Did not find person id - " + id);
 
-        if (changedPerson.getName() != null) {  //проверка на длину строки?
+        if (changedPerson.getName() != null) {
             person.setName(changedPerson.getName());
         }
 
@@ -67,10 +70,15 @@ public class PersonService {
         }
 
         if (changedPerson.getPassport() != null) {
+            var allPeople = personRepository.findAll();
+            for (Person basePerson : allPeople) {
+                if (basePerson.getPassport().equals(changedPerson.getPassport()))
+                    throw new RuntimeException("Person with passport: " + person.getPassport() + " already exists");
+            }
             person.setPassport(changedPerson.getPassport());
         }
 
-        if (changedPerson.getVehicles() != null) {             //мб убрать
+        if (changedPerson.getVehicles() != null) {
             person.setVehicles(changedPerson.getVehicles());
         }
 
@@ -78,21 +86,46 @@ public class PersonService {
     }
 
     public void assignVehicleToPerson(int id, int vin) {
-        Person person = personRepository.findById(id).get(); //orElseThrow(ex)
-        Vehicle vehicle = vehicleRepository.findById(vin).get();
+        Optional<Person> resultPerson = personRepository.findById(id);
+        Person person;
+        if (resultPerson.isPresent()) {
+            person = resultPerson.get();
+        } else throw new ResourceNotFoundException("Did not find person id - " + id);
+
+        Optional<Vehicle> resultVehicle = vehicleRepository.findById(vin);
+        Vehicle vehicle;
+        if (resultVehicle.isPresent()) {
+            vehicle = resultVehicle.get();
+        } else throw new ResourceNotFoundException("Did not find vehicle vin - " + vin);
+
         person.getVehicles().add(vehicle);
         personRepository.save(person);
     }
 
     public void unAssignVehicleFromPerson(int id, int vin) {
-        Person person = personRepository.findById(id).get(); //orElseThrow(ex)
-        Vehicle vehicle = vehicleRepository.findById(vin).get();
+        Optional<Person> resultPerson = personRepository.findById(id);
+        Person person;
+        if (resultPerson.isPresent()) {
+            person = resultPerson.get();
+        } else throw new ResourceNotFoundException("Did not find person id - " + id);
+
+        Optional<Vehicle> resultVehicle = vehicleRepository.findById(vin);
+        Vehicle vehicle;
+        if (resultVehicle.isPresent()) {
+            vehicle = resultVehicle.get();
+        } else throw new ResourceNotFoundException("Did not find vehicle vin - " + vin);
+
         person.getVehicles().remove(vehicle);
         personRepository.save(person);
     }
 
     public Set<Vehicle> getPersonVehiclesByPersonId(int id) {
-        Person person = personRepository.findById(id).get();
+        Optional<Person> resultPerson = personRepository.findById(id);
+        Person person;
+        if (resultPerson.isPresent()) {
+            person = resultPerson.get();
+        } else throw new ResourceNotFoundException("Did not find person id - " + id);
+
         return person.getVehicles();
     }
 
--- src/main/java/org/gus/carbd/service/VehicleService.java
+++ src/main/java/org/gus/carbd/service/VehicleService.java
@@ -2,6 +2,7 @@ package org.gus.carbd.service;
 
 import org.gus.carbd.entity.Person;
 import org.gus.carbd.entity.Vehicle;
+import org.gus.carbd.exception.ResourceNotFoundException;
 import org.gus.carbd.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,11 +24,10 @@ public class VehicleService {
 
     public Vehicle getVehicleByVin(int vin) {
         Optional<Vehicle> result = vehicleRepository.findById(vin);
-        Vehicle vehicle = new Vehicle();
-
+        Vehicle vehicle;
         if (result.isPresent()) {
             vehicle = result.get();
-        }
+        } else throw new ResourceNotFoundException("Did not find vehicle vin - " + vin);
 
         return vehicle;
     }
@@ -42,25 +42,24 @@ public class VehicleService {
 
     public void editVehicleByVin(int vin, Vehicle changedVehicle) {
         Optional<Vehicle> result = vehicleRepository.findById(vin);
-
-        Vehicle vehicle = new Vehicle();
+        Vehicle vehicle;
         if (result.isPresent()) {
             vehicle = result.get();
-        }
+        } else throw new ResourceNotFoundException("Did not find vehicle vin - " + vin);
 
-        if (changedVehicle.getBrand()!= null) {  //проверка на длину строки?
+        if (changedVehicle.getBrand() != null) {
             vehicle.setBrand(changedVehicle.getBrand());
         }
 
-        if (changedVehicle.getModel()!= null) {
+        if (changedVehicle.getModel() != null) {
             vehicle.setModel(changedVehicle.getModel());
         }
 
-        if (changedVehicle.getYear()!= null) {
+        if (changedVehicle.getYear() != null) {
             vehicle.setYear(changedVehicle.getYear());
         }
 
-        if (changedVehicle.getPeople()!= null) {
+        if (changedVehicle.getPeople() != null) {
             vehicle.setPeople(vehicle.getPeople());
         }
 
@@ -68,15 +67,25 @@ public class VehicleService {
     }
 
     public Set<Person> getVehicleOwners(int vin) {
-        Vehicle vehicle = vehicleRepository.findById(vin).get();
+        Optional<Vehicle> result = vehicleRepository.findById(vin);
+        Vehicle vehicle;
+        if (result.isPresent()) {
+            vehicle = result.get();
+        } else throw new ResourceNotFoundException("Did not find vehicle vin - " + vin);
+
         return vehicle.getPeople();
     }
 
     public List<String> getVehicleOwnersPassports(int vin) {
-        Vehicle vehicle = vehicleRepository.findById(vin).get();
+        Optional<Vehicle> result = vehicleRepository.findById(vin);
+        Vehicle vehicle;
+        if (result.isPresent()) {
+            vehicle = result.get();
+        } else throw new ResourceNotFoundException("Did not find vehicle vin - " + vin);
+
         List<String> passportList = new ArrayList<>();
         var peopleSet = vehicle.getPeople();
-        for (Person person: peopleSet){
+        for (Person person : peopleSet) {
             passportList.add(person.getPassport());
         }
         return passportList;
